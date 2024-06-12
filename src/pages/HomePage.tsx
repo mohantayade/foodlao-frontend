85a6660a0ef9a57ebc15946c5da6675640f1d9a8
mohantayade <mohantayade326@gmail.com>
2024-06-12 10:06:49 +0530

    new clone

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,7 @@ function HomePage() {
     <div className="flex flex-col gap-12">
         <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
             <h1 className=" text-5xl font-bold tracking-tight text-orange-600">Truck In To TakeWay Today</h1>
-            <span className="text-xl">Food is just a click away!</span>
+            <span className="text-xl">Food is just a click away!!</span>
         </div>
         <div className="grid md:grid-cols-2 gap-5">
             <img src={phoneimage} alt="foodlao app image" className="rounded-lg" />
